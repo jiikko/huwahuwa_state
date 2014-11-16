5044116b58cc766ff92e195c9695562af723ddbe
jiikko <n905i.1214@gmail.com>
2014-11-16 19:36:07 +0900

    コッチノほうが安全そう

--- lib/huwahuwa_state.rb
+++ lib/huwahuwa_state.rb
@@ -1,4 +1,14 @@
 require "huwahuwa_state/version"
 require 'huwahuwa_state/main'
 
-::ActiveRecord::Base.send(:include, HuwahuwaState::Main)
+if defined?(Rails)
+  class Railtie < Rails::Railtie
+    initializer 'initialize huwahuwa_state' do
+      ActiveSupport.on_load(:active_record) do
+        ::ActiveRecord::Base.send(:include, HuwahuwaState::Main)
+      end
+    end
+  end
+else
+  ::ActiveRecord::Base.send(:include, HuwahuwaState::Main)
+end
